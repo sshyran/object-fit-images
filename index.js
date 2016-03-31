6170255cc6ebcaebf560e695fc63354ca150f315
Federico Brigante <bfred-it@users.noreply.github.com>
2016-04-01 00:46:25 +0700

    Added proper support for scale-down
    scale-down depends on the image natural size and calculated size, so
    watchMQ is suggested when scale-down is expected
    
    Fixes #9

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-var privateAccessor = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='; // transparent image, might as well reuse it
+var ಠ = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='; // transparent image, used as accessor and replacing image
 var propRegex = /(object-fit|object-position)\s*:\s*([^;$"'\s]+)/g;
 var isSupported = 'object-fit' in document.createElement('i').style;
 var autoModeEnabled = false;
@@ -26,10 +26,6 @@ function fixOne(el, src, style) {
 	// Edge 12 doesn't support currentSrc
 	// https://github.com/bfred-it/object-fit-images/blob/gh-pages/detailed-support-tables.md#object-fit-images--srcset
 	src = src || el.currentSrc || el.src;
-	el.style.backgroundImage = 'url(' + src + ')';
-	el.style.backgroundPosition = style['object-position'] || 'center';
-	el.style.backgroundSize = style['object-fit'].replace('scale-down', 'contain'); // "object-fit: none" automatically maps to "background-size:auto"
-	el.style.backgroundRepeat = 'no-repeat';
 
 	// remove srcset because it overrides src
 	if (el.srcset) {
@@ -37,21 +33,53 @@ function fixOne(el, src, style) {
 	}
 
 	// if it hadn't been already activated
-	if (!el[privateAccessor]) {
-		el.src = privateAccessor;
+	if (!el[ಠ]) {
+		el.src = ಠ;
 		keepSrcUsable(el);
 	}
 
-	// remember real image url
-	el[privateAccessor] = src;
+	// store info on object for later use
+	el[ಠ] = el[ಠ] || {s: src};
+
+	el.style.backgroundImage = 'url(' + src + ')';
+	el.style.backgroundPosition = style['object-position'] || 'center';
+	el.style.backgroundRepeat = 'no-repeat';
+
+	if (style['object-fit'].indexOf('scale-down') < 0) {
+		el.style.backgroundSize = style['object-fit'].replace('none', 'auto');
+	} else {
+		// `object-fit: scale-down` is either `contain` or `auto`
+		if (!el[ಠ].i) {
+			el[ಠ].i = new Image();
+			el[ಠ].i.src = src;
+		}
+
+		// naturalWidth is only available when the image headers are loaded,
+		// this loop will poll it every 100ms.
+		// There's currently no check to prevent this loop from starting twice
+		// as a consequence of calling ofi() twice on the same image, but it's light
+		// and causes no issues, so it's not worth ensuring that it doesn't.
+		(function loop() {
+			if (el[ಠ].i.naturalWidth) {
+				if (el[ಠ].i.naturalWidth > el.width || el[ಠ].i.naturalHeight > el.height) {
+					el.style.backgroundSize = 'contain';
+				} else {
+					el.style.backgroundSize = 'auto';
+				}
+				return;
+			}
+			setTimeout(loop, 100);
+		})();
+	}
 }
 
 function keepSrcUsable(el) {
 	var definitions = {
 		get: function () {
-			return el[privateAccessor];
+			return el[ಠ].s;
 		},
 		set: function (v) {
+			delete el[ಠ].i; // scale-down's img sizes need to be updated too
 			return fixOne(el, v);
 		}
 	};
