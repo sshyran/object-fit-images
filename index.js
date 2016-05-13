0d28dec81f15158bfab170b92ae53c25c175c8ed
Federico Brigante <bfred-it@users.noreply.github.com>
2016-05-13 16:41:14 +0700

    Added support for multiple percentages in object-position

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 var ಠ = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='; // transparent image, used as accessor and replacing image
-var propRegex = /(object-fit|object-position)\s*:\s*([^;$"'\s]+)/g;
+var propRegex = /(object-fit|object-position)\s*:\s*([\w\s%]+)/g;
 var isSupported = 'object-fit' in document.createElement('i').style;
 var autoModeEnabled = false;
 
